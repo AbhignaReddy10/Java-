458cc8e955cb2e16947b0a14dedaec0c45261d11
AbhignaReddy10 <nabhigna1234@gmail.com>
2026-02-12 22:28:46 +0530

    Add files via upload

---
+++ addMod.java
@@ -0,0 +1,22 @@
+//aim: this program gives modulos of addition 
+import java.util.Scanner;
+class addMod {
+    public static int sumUnderModulo(int a, int b, int M) {
+       int sum=0;
+        
+         sum = a+b;
+         return sum%M;
+         
+    
+        
+    }
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int A =sc.nextInt();
+        int B = sc.nextInt();
+        int M = sc.nextInt();
+        
+        int sol = sumUnderModulo(A,B,M);
+        
+    }
+}
